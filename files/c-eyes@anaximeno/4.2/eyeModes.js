f9bfb85e1d5058f947d006f0e4c6eb0b40d2b68e
anaximeno <anaximenobrito@gmail.com>
2023-02-25 14:43:16 -0100

    feat: add new eye mode
    chore: v1.10.3

--- files/c-eyes@anaximeno/4.2/eyeModes.js
+++ files/c-eyes@anaximeno/4.2/eyeModes.js
@@ -161,6 +161,103 @@ class EyelidMode extends EyeMode {
     }
 }
 
+class EyelidFillMode extends EyeMode {
+    drawEye(area, options) {
+        let [area_x, area_y] = this.areaPos();
+        let [area_width, area_height] = area.get_surface_size();
+        let [mouse_x, mouse_y, _] = global.get_pointer();
+
+        area_x += area_width / 2;
+        area_y += area_height / 2;
+
+        mouse_x -= area_x;
+        mouse_y -= area_y;
+
+        let mouse_ang = Math.atan2(mouse_y, mouse_x);
+        let mouse_rad = Math.sqrt(mouse_x * mouse_x + mouse_y * mouse_y);
+
+        let eye_rad = (area_height) / 2;
+        let iris_rad = eye_rad * 0.5;
+        let pupil_rad = iris_rad * 0.4;
+
+        let max_rad = eye_rad * (Math.pow(Math.cos(mouse_ang), 4) * 0.5 + 0.25);
+
+        if (mouse_rad > max_rad)
+            mouse_rad = max_rad;
+
+        let iris_arc = Math.asin(iris_rad / eye_rad);
+        let iris_r = eye_rad * Math.cos(iris_arc);
+
+        let eye_ang = Math.atan(mouse_rad / iris_r);
+
+        let cr = area.get_context();
+
+        // -- Drawing the base of the eye
+
+        Clutter.cairo_set_source_color(cr, options.eye_color);
+
+        cr.translate(area_width * 0.5, area_height * 0.5);
+        cr.setLineWidth(this.eye.eye_line_width);
+
+        let x_def = iris_rad * Math.cos(mouse_ang) * (Math.sin(eye_ang));
+        let y_def = iris_rad * Math.sin(mouse_ang) * (Math.sin(eye_ang));
+        let amp;
+
+        let top_lid = 0.8;
+        let bottom_lid = 0.6;
+
+        amp = eye_rad * top_lid;
+        cr.moveTo(-eye_rad, 0);
+        cr.curveTo(x_def - iris_rad, y_def + amp,
+            x_def + iris_rad, y_def + amp, eye_rad, 0);
+
+        amp = eye_rad * bottom_lid;
+        cr.curveTo(x_def + iris_rad, y_def - amp,
+            x_def - iris_rad, y_def - amp, -eye_rad, 0);
+
+        this.eye.eye_activated ? cr.fill() : cr.stroke();
+
+        amp = eye_rad * top_lid;
+        cr.moveTo(-eye_rad, 0);
+        cr.curveTo(x_def - iris_rad, y_def + amp,
+            x_def + iris_rad, y_def + amp, eye_rad, 0);
+
+        amp = eye_rad * bottom_lid;
+        cr.curveTo(x_def + iris_rad, y_def - amp,
+            x_def - iris_rad, y_def - amp, -eye_rad, 0);
+        cr.clip();
+
+        // -- Drawing the iris of the eye
+
+        cr.rotate(mouse_ang);
+        cr.setLineWidth(this.eye.eye_line_width / iris_rad);
+
+        Clutter.cairo_set_source_color(cr, options.iris_color);
+
+        cr.translate(iris_r * Math.sin(eye_ang), 0);
+        cr.scale(iris_rad * Math.cos(eye_ang), iris_rad);
+        cr.arc(0, 0, 1.0, 0, 2 * Math.PI);
+
+        this.eye.eye_activated ? cr.fill() : cr.stroke();
+
+        cr.scale(1 / (iris_rad * Math.cos(eye_ang)), 1 / iris_rad);
+        cr.translate(-iris_r * Math.sin(eye_ang), 0);
+
+        // -- Drawing the pupil of the eye
+
+        Clutter.cairo_set_source_color(cr, options.pupil_color);
+
+        cr.translate(eye_rad * Math.sin(eye_ang), 0);
+        cr.scale(pupil_rad * Math.cos(eye_ang), pupil_rad);
+        cr.arc(0, 0, 1.0, 0, 2 * Math.PI);
+        cr.fill();
+
+        cr.save();
+        cr.restore();
+        cr.$dispose();
+    }
+}
+
 class BulbMode extends EyeMode {
     drawEye(area, options) {
         let [area_x, area_y] = this.areaPos();
@@ -242,6 +339,8 @@ class EyeModeFactory {
         switch (mode) {
             case "bulb":
                 return new BulbMode(eye);
+            case "lids-fill":
+                return new EyelidFillMode(eye);
             case "lids":
             default:
                 return new EyelidMode(eye);
